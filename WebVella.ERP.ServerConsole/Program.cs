dc5a9df456c9ecddddfb36b831c58c7b09b03906
rumen <rumen@webvella.com>
2015-02-25 14:51:16 +0200

    initial commit of the project skeleton

---
+++ WebVella.ERP.ServerConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebVella.ERP.ServerConsole
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
