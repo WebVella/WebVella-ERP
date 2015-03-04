1fc3a224a62393174c2e56e297d0640de56d93c2
Goro <goro@webvella.com>
2015-03-04 15:56:01 +0200

    Add MVC support

---
+++ WebVella.ERP.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace WebVella.ERP.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- WebVella.ERP.Web/Startup.cs
+++ WebVella.ERP.Web/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Routing;
 using Microsoft.Framework.Runtime.Infrastructure;
@@ -20,22 +22,52 @@ namespace WebVella.ERP.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddSingleton<IStorageService, MongoStorageService>();
             services.AddSingleton<IERPService, ERPService>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env )
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-           app.Run(async context =>
-           {
-               IERPService service = app.ApplicationServices.GetService<IERPService>();
-               service.Run();
-               context.Response.ContentType = "text/html";
-               context.Response.StatusCode = 200;
-               System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
-               byte[] buffer = encoding.GetBytes("<h1>test</h1>");
-               await context.Response.Body.WriteAsync(buffer, 0, buffer.Length);
-           });
+            //app.Run(async context =>
+            //{
+            //    IERPService service = app.ApplicationServices.GetService<IERPService>();
+            //    service.Run();
+            //    context.Response.ContentType = "text/html";
+            //    context.Response.StatusCode = 200;
+            //    System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            //    byte[] buffer = encoding.GetBytes("<h1>test</h1>");
+            //    await context.Response.Body.WriteAsync(buffer, 0, buffer.Length);
+            //});
+
+            // Add the following to the request pipeline only in development environment.
+            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+            {
+                app.UseBrowserLink();
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // send the request to the following path or controller action.
+                app.UseErrorHandler("/Home/Error");
+            }
+
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
         }
     }
 }
