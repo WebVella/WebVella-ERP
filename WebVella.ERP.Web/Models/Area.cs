0a1aa0bfb10e0df59fd678025234fc596b0d1b22
boz1 <boz@webvella.com>
2015-04-27 15:50:24 +0300

    Initial implementation for the Area and Admin UI Pages

---
+++ WebVella.ERP.Web/Models/Area.cs
@@ -0,0 +1,79 @@
+﻿//Test model for an Area
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace WebVella.ERP.Web.Models
+{
+    public class Area
+    {
+		public Area() {
+			Id = Guid.Empty;
+			Name = String.Empty;
+			Label = String.Empty;
+			Weight = 0;
+			Sections = new List<AreaSection>();
+		}
+
+		[JsonProperty(PropertyName = "id")]
+		public Guid Id { get; set; }
+
+		//Unique technical name of the entity, e.g. order
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+
+		//Human readable identifier of a single instance of this entity, e.g. Order
+		[JsonProperty(PropertyName = "label")]
+		public string Label { get; set; }
+
+		//Weight- way of sorting ares in the list, Bigger weight sinks the element down the list
+		[JsonProperty(PropertyName = "weight")]
+		public int Weight { get; set; }
+
+		//Whether the area has a dashboard page
+		[JsonProperty(PropertyName = "hasDashboard")]
+		public bool HasDashboard { get; set; }
+
+		//Whether the area has a search page
+		[JsonProperty(PropertyName = "hasSearch")]
+		public bool HasSearch { get; set; }
+
+		//Area Entity sections (grouping). Entities that need to be directly linked to the area must have section with id = Guid.Empty
+		[JsonProperty(PropertyName = "sections")]
+		public List<AreaSection> Sections { get; set; }
+
+	}
+
+	public class AreaSection
+	{
+		public AreaSection()
+		{
+			Id = Guid.Empty;
+			Name = String.Empty;
+			Label = String.Empty;
+			Weight = 0;
+			Entities = new List<Entity>();
+		}
+
+		[JsonProperty(PropertyName = "id")]
+		public Guid Id { get; set; }
+
+		//Unique technical name of the entity, e.g. order
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+
+		//Human readable identifier of a single instance of this entity, e.g. Order
+		[JsonProperty(PropertyName = "label")]
+		public string Label { get; set; }
+
+		//Weight- way of sorting ares in the list, Bigger weight sinks the element down the list
+		[JsonProperty(PropertyName = "weight")]
+		public int Weight { get; set; }
+
+		//Entities attached to the  section. Entities that need to be directly linked to the area must have section with id = Guid.Empty
+		[JsonProperty(PropertyName = "entities")]
+		public List<Entity> Entities { get; set; }
+
+	}
+}
--- WebVella.ERP.Web/wwwroot/app.js
+++ WebVella.ERP.Web/wwwroot/app.js
@@ -9,31 +9,34 @@
 
 var ErpApp = angular.module('ErpApp', [
     'ngMaterial', 'ngTouch', 'ui.router',
-    'dashboard', 'navigation'
+    'areaEntity', 'navigation',
+	'adminEntities'
 ]);
 
+
+//Application Configuration
 ErpApp.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
 	//Set to override the default theme pallete settings
 	$mdThemingProvider.theme( 'default' ).primaryPalette( 'indigo' ).accentPalette( 'grey' );
-	$urlRouterProvider.otherwise('/dashboard');
-}
-);
+	$urlRouterProvider.otherwise('/area/area1/null/dashboard');
 
-ErpApp.constant(
-	"$config", {
-		apiBaseUrl: "/api",
-		pageSize: 10
-	}
-);
+	//The URLs will be
+	//area - area entity data list
+	//admin - ERP administration
+	//entity - area entity manage
 
+});
 
-
-ErpApp.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+ErpApp.controller('AppController', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
 
 	//Side Menu Toggler
+	$scope.siteMeta = {};
 	$scope.toggleLeft = buildToggler('left');
 	$scope.toggleRight = buildToggler('right');
-	
+	$scope.$watch("siteMeta", function () {
+		$scope.$broadcast("siteMeta-changed");
+	});
+
 	function buildToggler(navID) {
 		var debounceFn = $mdUtil.debounce(function () {
 			$mdSidenav(navID)
@@ -47,3 +50,22 @@ ErpApp.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $l
 
 
 });
+
+var ResolveSiteMetaResponse = function ($q, $state, $config, $ApiService) {
+	// Initialize
+	var defer = $q.defer();
+
+	// Process
+	function successCallBack(response) {
+		defer.resolve(response);
+	}
+
+	function errorCallBack(response) {
+		defer.resolve(response);
+	}
+
+	$ApiService.getSiteMeta(successCallBack, errorCallBack);
+
+	// Return
+	return defer.promise;
+};
