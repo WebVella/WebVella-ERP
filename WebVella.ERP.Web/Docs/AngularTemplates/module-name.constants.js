a6ae66a1c53268a566d8d8749c3ef57026ef3746
boz <boz@webvella.com>
2015-05-04 08:02:18 -0700

    Preparing for the Interface change from Angular Material to Bootstrap Angular

---
+++ WebVella.ERP.Web/Docs/AngularTemplates/module-name.constants.js
@@ -0,0 +1,15 @@
+/* some-name.constants.js */
+
+/**
+* @desc just a sample contstants code
+*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .constant('constantName', constantValue);
+})();
+
+// Later can be used in controller as a dependency
