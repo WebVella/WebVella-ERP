0cdd17255ee00b48b933c1f1106b8adce697bf4c
boz <boz@webvella.com>
2015-05-11 12:01:56 +0300

    Developers module for testing

---
+++ WebVella.ERP.Web/wwwroot/plugins/webvella-developers/base.module.js
@@ -0,0 +1,84 @@
+﻿/* base.module.js */
+
+/**
+* @desc this the base module of the Desktop plugin. Its only tasks is to check the topNavFactory and redirect to the first menu item state
+*/
+
+(function () {
+    'use strict';
+
+    angular
+        .module('webvellaDevelopers', ['ui.router'])
+        .config(config)
+        .run(run)
+        .controller('WebVellaDevelopersBaseController', controller);
+
+    // Configuration ///////////////////////////////////
+    config.$inject = ['$stateProvider'];
+
+    /* @ngInject */
+    function config($stateProvider) {
+        $stateProvider.state('webvella-developers-base', {
+            //abstract: true,
+            parent: 'webvella-root',
+            url: '/developers', //will be added to all children states
+            views: {
+                "pluginView": {
+                    controller: 'WebVellaDevelopersBaseController',
+                    templateUrl: '/plugins/webvella-developers/base.view.html',
+                    controllerAs: 'pluginData'
+                }
+            },
+            resolve: {
+                //here you can resolve any plugin wide data you need. It will be available for all children states. Parent resolved objects can be injected in the functions too
+                pageTitle: function (pageTitle) {
+                    return "Developers | " + pageTitle;
+                }
+            },
+            data: { }
+        });
+    };
+
+
+    // Run //////////////////////////////////////
+    run.$inject = ['$log', '$rootScope', 'webvellaDesktopBrowsenavFactory'];
+
+    /* @ngInject */
+    function run($log, $rootScope, webvellaDesktopBrowsenavFactory) {
+        $log.debug('webvellaDevlopers>base> BEGIN module.run');
+        $rootScope.$on('webvellaDesktop-browsenav-ready', function (event) {
+            var item = {
+                "label": "Developers",
+                "stateName": "webvella-developers-base",
+                "stateParams": {},
+                "parentName": "",
+                "nodes": [],
+                "weight": 101.0,
+                "color": "purple",
+                "iconName": "code"
+            };
+
+            webvellaDesktopBrowsenavFactory.addItem(item);
+        });
+        $log.debug('webvellaDevelopers>base> END module.run');
+    };
+
+
+    // Controller ///////////////////////////////
+    controller.$inject = ['$log'];
+
+    /* @ngInject */
+    function controller($log) {
+        $log.debug('webvellaDevelopers>base> BEGIN controller.exec');
+        /* jshint validthis:true */
+        var pluginData = this;
+   
+
+        $log.debug('webvellaDevelopers>base> END controller.exec');
+
+        function activate() {
+
+        }
+    }
+
+})();
