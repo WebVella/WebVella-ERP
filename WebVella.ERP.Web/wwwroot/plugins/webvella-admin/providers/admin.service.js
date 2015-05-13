e99d9571f321c76a8ca19b66a8caa58626ed67dc
boz <boz@webvella.com>
2015-05-13 17:15:24 +0300

    Field implementation

--- WebVella.ERP.Web/wwwroot/plugins/webvella-admin/providers/admin.service.js
+++ WebVella.ERP.Web/wwwroot/plugins/webvella-admin/providers/admin.service.js
@@ -18,15 +18,42 @@
         var serviceInstance = this;
 
         serviceInstance.getMetaEntityList = getMetaEntityList;
+        serviceInstance.initEntity = initEntity;
         serviceInstance.createEntity = createEntity;
         serviceInstance.getEntityMeta = getEntityMeta;
         serviceInstance.deleteEntity = deleteEntity;
+        serviceInstance.initField = initField;
+        serviceInstance.createField = createField;
+
+        //#region << Entity >>
 
         ///////////////////////
         function getMetaEntityList(successCallback, errorCallback) {
             $log.debug('webvellaAdmin>providers>admin.service>getMetaEntityList> function called');
             $http({ method: 'GET', url: wvAppConstants.apiBaseUrl + 'meta/entity/list' }).success(function (data, status, headers, config) { handleSuccessResult(data, status, successCallback, errorCallback); }).error(function (data, status, headers, config) { handleErrorResult(data, status, errorCallback); });
         }
+
+        ///////////////////////
+        function initEntity() {
+            $log.debug('webvellaAdmin>providers>admin.service>initEntity> function called');
+            var entity = {
+                id: null,
+                name: "",
+                label: "",
+                pluralLabel: "",
+                system: false,
+                iconName: "database",
+                weight: 1.0,
+                recordPermissions: {
+                    canRead: [],
+                    canCreate: [],
+                    canUpdate: [],
+                    canDelete: []
+                }
+            };
+            return entity;
+        }
+
 
         ///////////////////////
         function createEntity(postObject, successCallback, errorCallback) {
@@ -46,8 +73,136 @@
             $http({ method: 'DELETE', url: wvAppConstants.apiBaseUrl + 'meta/entity/' + entityId }).success(function (data, status, headers, config) { handleSuccessResult(data, status, successCallback, errorCallback); }).error(function (data, status, headers, config) { handleErrorResult(data, status, errorCallback); });
         }
 
+        //#endregion << Entity >>
 
-        //// Aux methods //////////////////////////////////////////////////////
+        //#region << Field >>
+
+
+        ///////////////////////
+        function initField(typeId) {
+            $log.debug('webvellaAdmin>providers>admin.service>initField> function called');
+            var field = {
+                id: null,
+                name: "",
+                label: "",
+                placeholderText: "",
+                description: "",
+                helpText: "",
+                required: false,
+                unique: false,
+                searchable: false,
+                auditable: false,
+                system: false,
+                fieldType: typeId,
+            };
+
+            switch (typeId) {
+                case 1:
+                    field.defaultValue = 1.0;
+                    field.startingNumber = 1.0;
+                    field.displayFormat = "";
+                    break;
+                case 2:
+                    field.defaultValue = false;
+                    break;
+                case 3:
+                    field.defaultValue = 1.0;
+                    field.minValue = 1.0;
+                    field.maxValue = 1.0;
+                    field.currency = {
+                        currencySymbol: "",
+                        currencyName: "",
+                        position:0
+                    };
+                    break;
+                case 4:
+                    field.defaultValue = '2000-00-01T00:00:01.000Z';
+                    field.format = "";
+                    field.useCurrentTimeAsDefaultValue = false;
+                    break;
+                case 5:
+                    field.defaultValue = '2000-00-01T00:00:01.000Z';
+                    field.format = "";
+                    field.useCurrentTimeAsDefaultValue = false;
+                    break;
+                case 6:
+                    field.defaultValue = '';
+                    field.maxLength = 150;
+                    break;
+                case 7:
+                    field.defaultValue = '';
+                    break;
+                case 8:
+                    field.defaultValue = '';
+                    break;
+                case 9:
+                    field.defaultValue = '';
+                    break;
+                case 10:
+                    field.defaultValue = '';
+                    field.maxLength = 1000;
+                    field.visibleLineNumber = false;
+                    break;
+                case 11:
+                    field.defaultValue = '';
+                    field.options = [];
+                    break;
+                case 12:
+                    field.defaultValue = 1.0;
+                    field.minValue = 1.0;
+                    field.maxValue = 1.0;
+                    field.decimalPlaces = 2;
+                    break;
+                case 13:
+                    field.maxLength = 50;
+                    field.encrypted = true;
+                    field.maskType = 1;
+                    field.maskCharacter = null;
+                    break;
+                case 14:
+                    field.defaultValue = 1.0;
+                    field.minValue = 0.0;
+                    field.maxValue = 100.0;
+                    field.decimalPlaces = 2;
+                    break;
+                case 15:
+                    field.defaultValue = "";
+                    field.format = "";
+                    field.maxLength = 50;
+                    break;
+                case 16:
+                    field.defaultValue = "00000000-0000-0000-0000-000000000000";
+                    break;
+                case 17:
+                    field.defaultValue = "";
+                    field.options = [];
+                    break;
+                case 18:
+                    field.defaultValue = "";
+                    field.maxLength = 50;
+                    break;
+                case 19:
+                    field.defaultValue = "";
+                    field.maxLength = 50;
+                    field.openTargetInNewWindow = false;
+                    break;
+                default:
+                    break;
+            }
+
+            return field;
+        }
+
+
+        ///////////////////////
+        function createField(postObject, entityId, successCallback, errorCallback) {
+            $log.debug('webvellaAdmin>providers>admin.service>createField> function called');
+            $http({ method: 'POST', url: wvAppConstants.apiBaseUrl + 'meta/entity/' + entityId + '/field', data: postObject }).success(function (data, status, headers, config) { handleSuccessResult(data, status, successCallback, errorCallback); }).error(function (data, status, headers, config) { handleErrorResult(data, status, errorCallback); });
+        }
+
+        //#endregion
+
+        //#region << Aux methods >>
 
         // Global functions for result handling for all methods of this service
         function handleErrorResult(data, status, errorCallback) {
@@ -89,6 +244,6 @@
                 successCallback(data);
             }
         }
-
+        //#endregion
     }
 })();
