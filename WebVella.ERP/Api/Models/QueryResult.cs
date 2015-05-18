72c963f7e3479a26bc7a328e91c3c846b4367ed8
rumen-yankov <rumen.yankov@gmail.com>
2015-05-18 19:15:34 +0300

    adds query functionality implementation

--- WebVella.ERP/Api/Models/QueryResult.cs
+++ WebVella.ERP/Api/Models/QueryResult.cs
@@ -6,7 +6,7 @@ namespace WebVella.ERP.Api.Models
     public class QueryResult
     {
         [JsonProperty(PropertyName = "fieldsMeta")]
-        public List<Field> FieldsMeta { get; set; }
+        public List<Field> FieldsMeta { get; set; } 
 
         [JsonProperty(PropertyName = "data")]
         public List<EntityRecord> Data { get; set; }
