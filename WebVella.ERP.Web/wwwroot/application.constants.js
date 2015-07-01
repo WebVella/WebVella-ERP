925c5d85d671c94d284d090dd767a9518fb0ff17
boz <boz@webvella.com>
2015-07-01 15:55:05 +0300

    View details data management

--- WebVella.ERP.Web/wwwroot/application.constants.js
+++ WebVella.ERP.Web/wwwroot/application.constants.js
@@ -12,8 +12,51 @@
             "debugEnabled": false,
             "apiBaseUrl": "/api/v1/en_US/",
             "apiSandboxBaseUrl": "/sandbox/api",
-            "locale":"en_US"
+            "locale": "en_US"
         });
 })();
+
+function checkInt(data) {
+	var response = {
+		success: true,
+		message: "It is integer"
+	}
+
+	if (!isNaN(parseFloat(data)) && !isFinite(data)) {
+		response.success = false;
+		response.message = "Only integer is accepted";
+		return response;
+	}
+
+	if (data == parseInt(data, 10)) {
+		return response;
+	}
+	else {
+		response.success = false;
+		response.message = "Only integer is accepted";
+		return response;
+	}
+
+}
+
+
+function checkDecimal(data) {
+	var response = {
+		success: true,
+		message: "It is decimal"
+	}
+	if (data.toString().indexOf(",") > -1) {
+		response.success = false;
+		response.message = "Comma is not allowed. Use '.' for decimal separator";
+		return response;
+	}
+	if (!isNaN(parseFloat(data)) && !isFinite(data)) {
+		response.success = false;
+		response.message = "Only decimal is accepted";
+		return response;
+	}
+
+	return response;
+}
 
 // Later can be used in controller as a dependency
