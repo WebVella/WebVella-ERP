8e55390095129979149a11c75872f15d8d6f09d7
Rumen Yankov <rumen.yankov@gmail.com>
2015-08-18 16:56:27 +0300

    project converted for Visual Studio RTM version and beta5 packages

--- WebVella.ERP.Storage.Mongo/MongoStaticContext.cs
+++ WebVella.ERP.Storage.Mongo/MongoStaticContext.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MongoDB.Driver;
-using Microsoft.Framework.ConfigurationModel;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 
