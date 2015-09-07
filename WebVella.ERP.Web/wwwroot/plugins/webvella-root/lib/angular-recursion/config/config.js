4f60cea7780e439a600c0b401a39e23ad9d7d864
Boz Zashev <boz@webvella.com>
2015-09-07 09:32:47 +0300

    SubView recursive directive

---
+++ WebVella.ERP.Web/wwwroot/plugins/webvella-root/lib/angular-recursion/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testFiles: [
+		'bower_components/jQuery/dist/jquery.min.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'angular-recursion.min.js',
+        'test/**/*.js'
+    ]
+};
