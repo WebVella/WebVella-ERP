85dfea96ce6712ae673befe3c51c33ef7915d8d3
Boz Zashev <boz@webvella.com>
2015-10-05 08:39:16 +0300

    Login improvement

--- WebVella.ERP.Web/wwwroot/application.controller.js
+++ WebVella.ERP.Web/wwwroot/application.controller.js
@@ -5,122 +5,119 @@
 */
 
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
+	angular
         .module('wvApp')
         .config(config)
         .controller('ApplicationController', controller);
 
 
-    // Configuration ///////////////////////////////////
-    config.$inject = ['$httpProvider', 'wvAppConstants'];
+	// Configuration ///////////////////////////////////
+	config.$inject = ['$httpProvider', 'wvAppConstants'];
 
-    /* @ngInject */
-    function config($httpProvider, wvAppConstants) {
+	/* @ngInject */
+	function config($httpProvider, wvAppConstants) {
 
-    	$httpProvider.interceptors.push(function ($q, $window, ngToast, $cookies,$rootScope) {
-        	return {
-        		'request': function (request) {
-        			if (request.url.indexOf(wvAppConstants.apiBaseUrl) > -1) {
-        				if (request.url.indexOf("?") > -1) {
-        					//there are query string params in the request
-        					request.url = request.url + "&v=" + moment().format("YYYYMMDDHHmmssSSS");
-        				}
-        				else {
-        					//there are no query strings in the params
-        					request.url = request.url + "?v=" + moment().format("YYYYMMDDHHmmssSSS");
-        				}
-        			}
-        			else if (request.url.indexOf("/plugins/") > -1) {
-        				if (request.url.indexOf("?") > -1) {
-        					//there are query string params in the request
-        					request.url = request.url + "&v=" + wvAppConstants.htmlCacheBreaker;
-        				}
-        				else {
-        					//there are no query strings in the params
-        					request.url = request.url + "?v=" + wvAppConstants.htmlCacheBreaker;
-        				}
-        			}
+		$httpProvider.interceptors.push(function ($q, $location, ngToast, $cookies, $rootScope) {
+			return {
+				'request': function (request) {
+					if (request.url.indexOf(wvAppConstants.apiBaseUrl) > -1) {
+						if (request.url.indexOf("?") > -1) {
+							//there are query string params in the request
+							request.url = request.url + "&v=" + moment().format("YYYYMMDDHHmmssSSS");
+						}
+						else {
+							//there are no query strings in the params
+							request.url = request.url + "?v=" + moment().format("YYYYMMDDHHmmssSSS");
+						}
+					}
+					else if (request.url.indexOf("/plugins/") > -1) {
+						if (request.url.indexOf("?") > -1) {
+							//there are query string params in the request
+							request.url = request.url + "&v=" + wvAppConstants.htmlCacheBreaker;
+						}
+						else {
+							//there are no query strings in the params
+							request.url = request.url + "?v=" + wvAppConstants.htmlCacheBreaker;
+						}
+					}
 
-        			return $q.resolve(request);
-        		},
-                'responseError': function (errorResponse) {
-                	switch (errorResponse.status) {
-                		case 401:
-                			//Check if already called if yes do not call redirect or show message
-                			if (!$rootScope.notAuthorizedAlreadyProcessed) {
-                				ngToast.create({
-                					className: 'error',
-                					content: '<span class="go-red">Error code ' + errorResponse.status + '</span> ' + errorResponse.statusText
-                				});
-                				var cookieValue = $cookies.remove("erp-auth");
-                				$rootScope.notAuthorizedAlreadyProcessed = true;
-                				//$state.go("webvella-root-login");
-                				$window.location = '#/login';
-                			}
-                		case 403:
-                			ngToast.create({
-                				className: 'error',
-                				content: '<span class="go-red">Error code ' + errorResponse.status + '</span> ' + errorResponse.statusText
-                			});
-                			return $q.reject(errorResponse);
-                	}
-                }
-            }
-        });
-    };
+					return $q.resolve(request);
+				},
+				'responseError': function (errorResponse) {
+					if (errorResponse.status === 401) {
+						//Check if already called if yes do not call redirect or show message
+						ngToast.create({
+							className: 'error',
+							content: '<span class="go-red">Error code ' + errorResponse.status + '</span> ' + errorResponse.statusText
+						});
+						var cookieValue = $cookies.remove("erp-auth");
+						$location.path("/login");//.search({ returnUrl: angular.toJson(document.URL) });
+						return $q.reject(errorResponse);
+					}
+					else if (errorResponse.status === 403) {
+						ngToast.create({
+							className: 'error',
+							content: '<span class="go-red">Error code ' + errorResponse.status + '</span> ' + errorResponse.statusText
+						});
+						return $q.reject(errorResponse);
+					}
+				}
+			}
+		});
+	}
 
 
-    // Controller ///////////////////////////////
-    controller.$inject = ['$rootScope', '$log', '$cookies', '$localStorage', '$timeout', '$state', 'webvellaRootService'];
+	// Controller ///////////////////////////////
+	controller.$inject = ['$rootScope', '$log', '$cookies', '$localStorage', '$timeout', '$state', 'webvellaRootService'];
 
-    /* @ngInject */
-    function controller($rootScope, $log, $cookies, $localStorage, $timeout, $state, webvellaRootService) {
-    	$log.debug('vwApp> BEGIN controller.exec ' + moment().format('HH:mm:ss SSSS'));
-        /* jshint validthis:true */
-        var appData = this;
-        //Set page title
-        appData.pageTitle = 'WebVella ERP';
-        $rootScope.$on("application-pageTitle-update", function (event, newValue) {
-            appData.pageTitle = newValue;
-        });
-        //Set the body color
-        appData.bodyColor = "no-color";
-        $rootScope.$on("application-body-color-update", function (event, color) {
-            appData.bodyColor = color;
-        });
-        //Side menu toggle
-        appData.$storage = $localStorage;
-        if (!appData.$storage.isMiniSidebar) {
-            appData.$storage.isMiniSidebar = false;
-        }
-        //appData.isMiniSidebar = false;
-        //$rootScope.isMiniSidebar = false;
-        //if ($cookies.get("isMiniSidebar") == "true") {
-        //	appData.isMiniSidebar = true;
-        //	$rootScope.isMiniSidebar = true;
-        //}
+	/* @ngInject */
+	function controller($rootScope, $log, $cookies, $localStorage, $timeout, $state, webvellaRootService) {
+		$log.debug('vwApp> BEGIN controller.exec ' + moment().format('HH:mm:ss SSSS'));
+		/* jshint validthis:true */
+		var appData = this;
+		//Set page title
+		appData.pageTitle = 'WebVella ERP';
+		$rootScope.$on("application-pageTitle-update", function (event, newValue) {
+			appData.pageTitle = newValue;
+		});
+		//Set the body color
+		appData.bodyColor = "no-color";
+		$rootScope.$on("application-body-color-update", function (event, color) {
+			appData.bodyColor = color;
+		});
+		//Side menu toggle
+		appData.$storage = $localStorage;
+		if (!appData.$storage.isMiniSidebar) {
+			appData.$storage.isMiniSidebar = false;
+		}
+		//appData.isMiniSidebar = false;
+		//$rootScope.isMiniSidebar = false;
+		//if ($cookies.get("isMiniSidebar") == "true") {
+		//	appData.isMiniSidebar = true;
+		//	$rootScope.isMiniSidebar = true;
+		//}
 
-        //$rootScope.$on("application-sidebar-mini-toggle", function (event) {
-        //	appData.isMiniSidebar = !appData.isMiniSidebar;
-        //	$rootScope.isMiniSidebar = appData.isMiniSidebar;
-        //	$cookies.put("isMiniSidebar", appData.isMiniSidebar);
-        //});
-        //Side menu visibility
-        appData.sideMenuIsVisible = true;
-        $rootScope.$on("application-body-sidebar-menu-isVisible-update", function (event, isVisible) {
-            appData.sideMenuIsVisible = isVisible;
-        });
+		//$rootScope.$on("application-sidebar-mini-toggle", function (event) {
+		//	appData.isMiniSidebar = !appData.isMiniSidebar;
+		//	$rootScope.isMiniSidebar = appData.isMiniSidebar;
+		//	$cookies.put("isMiniSidebar", appData.isMiniSidebar);
+		//});
+		//Side menu visibility
+		appData.sideMenuIsVisible = true;
+		$rootScope.$on("application-body-sidebar-menu-isVisible-update", function (event, isVisible) {
+			appData.sideMenuIsVisible = isVisible;
+		});
 
-        //Redirect State (usefull when you need to redirect from resolve)
-        $rootScope.$on("state-change-needed", function (event, stateName, stateParams) {
-            $timeout(function () {
-                $state.go(stateName, stateParams, { reload: true });
-            }, 0);
-        });
+		//Redirect State (usefull when you need to redirect from resolve)
+		$rootScope.$on("state-change-needed", function (event, stateName, stateParams) {
+			$timeout(function () {
+				$state.go(stateName, stateParams, { reload: true });
+			}, 0);
+		});
 
-        $log.debug('wvApp> END controller.exec ' + moment().format('HH:mm:ss SSSS'));
-    }
+		$log.debug('wvApp> END controller.exec ' + moment().format('HH:mm:ss SSSS'));
+	}
 
 })();
--- WebVella.ERP.Web/wwwroot/plugins/webvella-root/login.module.js
+++ WebVella.ERP.Web/wwwroot/plugins/webvella-root/login.module.js
@@ -5,86 +5,74 @@
 */
 
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
+	angular
         .module('webvellaRoot')  //only gets the module, already initialized in the base.module of the plugin. The lack of dependency [] makes the difference.
         .config(config)
         .run(run)
         .controller('WebVellaRootLoginController', controller);
 
-    // Configuration ///////////////////////////////////
-    config.$inject = ['$stateProvider'];
-    /* @ngInject */
-    function config($stateProvider) {
-    	$stateProvider.state('webvella-root-login', {
-        	url: '/login',
-            views: {
-                "rootView": {
-                    controller: 'WebVellaRootLoginController',
-                    templateUrl: '/plugins/webvella-root/login.view.html',
-                    controllerAs: 'loginData'
-                }
-            },
-            resolve: {
-            	pageTitle: function () {
-            		return "Webvella ERP";
-            	}
-            }
-        });
-    };
+	// Configuration ///////////////////////////////////
+	config.$inject = ['$stateProvider'];
+	/* @ngInject */
+	function config($stateProvider) {
+		$stateProvider.state('webvella-root-login', {
+			url: '/login?returnUrl',
+			views: {
+				"rootView": {
+					controller: 'WebVellaRootLoginController',
+					templateUrl: '/plugins/webvella-root/login.view.html',
+					controllerAs: 'loginData'
+				}
+			},
+			resolve: {
+				pageTitle: function () {
+					return "Webvella ERP";
+				}
+			}
+		});
+	};
 
-    // Run //////////////////////////////////////
-    run.$inject = ['$log'];
-    /* @ngInject */
-    function run($log) {
-    	$log.debug('webvellaRoot>login> BEGIN module.run ' + moment().format('HH:mm:ss SSSS'));
+	// Run //////////////////////////////////////
+	run.$inject = ['$log'];
+	/* @ngInject */
+	function run($log) {
+		$log.debug('webvellaRoot>login> BEGIN module.run ' + moment().format('HH:mm:ss SSSS'));
 
-    	$log.debug('webvellaRoot>login> END module.run ' + moment().format('HH:mm:ss SSSS'));
-    };
+		$log.debug('webvellaRoot>login> END module.run ' + moment().format('HH:mm:ss SSSS'));
+	};
 
 
-    // Controller ///////////////////////////////
-    controller.$inject = ['$state', '$log', 'webvellaRootService', '$timeout', 'pageTitle'];
+	// Controller ///////////////////////////////
+	controller.$inject = ['$state', '$log', 'webvellaRootService', '$timeout', 'pageTitle', '$stateParams', '$location'];
 
-    /* @ngInject */
-    function controller($state, $log, webvellaRootService, $timeout, pageTitle) {
-    	$log.debug('webvellaRoot>login> BEGIN controller.exec ' + moment().format('HH:mm:ss SSSS'));
-        /* jshint validthis:true */
-    	var loginData = this;
-    	loginData.loginIsActive = false;
-    	//var currentUser = webvellaRootService.getCurrentUser();
-    	//if (currentUser != null) {
-    	//	$timeout(function () {
-    	//		$state.go("webvella-desktop-browse");
-    	//	}, 0);
-    	//}
-    	//else {
-    		loginData.loginIsActive = true;
+	/* @ngInject */
+	function controller($state, $log, webvellaRootService, $timeout, pageTitle, $stateParams, $location) {
+		$log.debug('webvellaRoot>login> BEGIN controller.exec ' + moment().format('HH:mm:ss SSSS'));
+		/* jshint validthis:true */
+		var loginData = this;
+		loginData.rememberMe = false;
+		loginData.pageTitle = "Login | " + pageTitle;
+		webvellaRootService.setPageTitle(loginData.pageTitle);
 
-    		//loginData.email = "erp@webvella.com";
-    		//loginData.password = "erp";
-    		loginData.rememberMe = false;
-    		loginData.pageTitle = "Login | " + pageTitle;
-    		webvellaRootService.setPageTitle(loginData.pageTitle);
+		loginData.ValidationErrors = false;
 
-    		loginData.ValidationErrors = false;
+		webvellaRootService.setPageTitle(loginData.pageTitle);
 
-    		webvellaRootService.setPageTitle(loginData.pageTitle);
+		loginData.doLogin = function () {
+			webvellaRootService.login(loginData,
+									  function (response) {
+									  	$timeout(function () {
+									  		$state.go('webvella-desktop-browse');
+									  	}, 10);
+									  },
+									  function (response) {
+									  	//show validation
+									  });
+		}
 
-    		loginData.doLogin = function () {
-    			webvellaRootService.login(loginData,
-										  function (response) {
-										  	$timeout(function () {
-										  		$state.go('webvella-desktop-browse');
-										  	}, 0);
-										  },
-										  function (response) {
-										  	//show validation
-										  });
-    		}
-    	//}
-    	$log.debug('webvellaRoot>login> END controller.exec ' + moment().format('HH:mm:ss SSSS'));
-    }
+		$log.debug('webvellaRoot>login> END controller.exec ' + moment().format('HH:mm:ss SSSS'));
+	}
 
 })();
