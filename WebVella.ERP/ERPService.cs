a9f31f03b97c5c8d8aeea83c92afda87e19614fa
Rumen Yankov <rumen.yankov@gmail.com>
2016-04-06 17:11:44 +0300

    fixes user password unique requirement

--- WebVella.ERP/ERPService.cs
+++ WebVella.ERP/ERPService.cs
@@ -152,7 +152,7 @@ namespace WebVella.ERP
 							password.Description = "Password for the user account";
 							password.HelpText = "";
 							password.Required = true;
-							password.Unique = true;
+							password.Unique = false;
 							password.Searchable = false;
 							password.Auditable = false;
 							password.System = true;
