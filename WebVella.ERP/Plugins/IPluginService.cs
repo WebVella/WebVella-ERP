7d16d6015299a1cad85de4b939a7af55e38f9f89
Rumen Yankov <rumen.yankov@gmail.com>
2016-04-26 15:56:24 +0300

    adds plugin support and sample plugin project

---
+++ WebVella.ERP/Plugins/IPluginService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc.Infrastructure;
+using System.Collections.Generic;
+using WebVella.ERP.Plugins;
+using WebVella.ERP.Storage;
+
+namespace WebVella.ERP.Plugins
+{
+    public interface IPluginService
+	{
+		List<Plugin> Plugins { get; }
+
+		void Initialize(IHostingEnvironment hostingEnvironment, IAssemblyProvider asmProviderService);
+    }
+}
---
+++ WebVella.ERP/Plugins/PluginService.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc.Infrastructure;
+using Microsoft.Extensions.PlatformAbstractions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace WebVella.ERP.Plugins
+{
+	public class PluginService : IPluginService
+	{
+		static List<Plugin> plugins = new List<Plugin>();
+		public List<Plugin> Plugins { get { return plugins; } }
+
+
+
+		public void Initialize(IHostingEnvironment hostingEnvironment, IAssemblyProvider asmProviderService)
+		{
+			var content = hostingEnvironment.WebRootFileProvider.GetDirectoryContents("/plugins");
+			if (!content.Exists)
+				return;
+
+
+			foreach (var pluginDir in content.Where(x => x.IsDirectory))
+			{
+				var manifestFilePath = Path.Combine(pluginDir.PhysicalPath, "manifest.json");
+				var manifestFile = new FileInfo(manifestFilePath);
+				if (!manifestFile.Exists)
+					continue;
+
+				var manifestJson = manifestFile.OpenText().ReadToEnd();
+				Plugin plugin = null;
+				try
+				{
+					plugin = JsonConvert.DeserializeObject<Plugin>(manifestJson);
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("An exception is thrown while parsing plugin manifest file: '" + manifestFilePath +"'", ex);
+				}
+				plugin.Assemblies = new List<Assembly>();
+
+				var binDir = new DirectoryInfo(Path.Combine(pluginDir.PhysicalPath, "bin"));
+				if (!binDir.Exists)
+					continue;
+
+				plugin.Assemblies.AddRange(GetAssembliesInFolder(binDir));
+
+				//we are working with assembly names (not the assemply instances),
+				//because in case of development mode, referenced assembly is loaded 
+				//we don't want to call 2 or more times start for assembly with same name
+				List<string> processedPlugins = new List<string>();
+				foreach (var assembly in asmProviderService.CandidateAssemblies)
+				{
+					if (processedPlugins.Contains(assembly.FullName))
+						continue;
+
+					if (plugin.Assemblies.Any(x => x.FullName == assembly.FullName))
+					{
+						processedPlugins.Add(assembly.FullName);
+						foreach (Type type in assembly.GetTypes())
+						{
+							if (type.GetCustomAttributes(typeof(PluginStartupAttribute), true).Length > 0)
+							{
+								try {
+									var obj = Activator.CreateInstance(assembly.FullName, type.Namespace + "." + type.Name).Unwrap();
+									var method = type.GetMethod("Start");
+									if (method != null)
+										method.Invoke(obj, null);
+								}
+								catch( Exception ex )
+								{
+									throw new Exception("An exception is thrown while execute start for plugin : '" +
+									 assembly.FullName + ";" + type.Namespace + "." + type.Name + "'", ex);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private IEnumerable<Assembly> GetAssembliesInFolder(DirectoryInfo binPath)
+		{
+			List<Assembly> assemblies = new List<Assembly>();
+			var loadContext = PlatformServices.Default.AssemblyLoadContextAccessor.Default;
+			PlatformServices.Default.AssemblyLoaderContainer.AddLoader(new PluginDirectoryLoader(binPath, loadContext));
+
+			foreach (var fileSystemInfo in binPath.GetFileSystemInfos("*.dll"))
+			{
+				var assembly = loadContext.Load(AssemblyName.GetAssemblyName(fileSystemInfo.FullName));
+				assemblies.Add(assembly);
+			}
+
+			return assemblies;
+		}
+	}
+}
