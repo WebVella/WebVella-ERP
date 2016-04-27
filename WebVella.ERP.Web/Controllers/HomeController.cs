3c46df96a7f7476d881076ccfe52f9587db6fa20
Boz Zashev <boz@webvella.com>
2016-04-27 12:32:08 +0300

    Enabling plugin manifest loading for the JS and CSS too.

--- WebVella.ERP.Web/Controllers/HomeController.cs
+++ WebVella.ERP.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
 using WebVella.ERP.Plugins;
 
 namespace WebVella.ERP.Web.Controllers
@@ -17,9 +18,46 @@ namespace WebVella.ERP.Web.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+			//CSS files
+			var libraryCssFileList = new List<string>();
+			var moduleCssFileList = new List<string>();
+			//JS files
+			var libraryJsFileList = new List<string>();
+			var moduleJsFileList = new List<string>();
+			//App dependency injections
+			var appDependencyInjections = new List<string>();
+
+			#region << Get and init plugin data >>
+			var pluginManifests = pluginService.Plugins;
+			pluginManifests.Sort((plugin1,plugin2) => plugin1.LoadPriority.CompareTo(plugin2.LoadPriority));
+			foreach(var manifest in pluginManifests) {
+				foreach(var cssFile in manifest.Libraries.Css) {
+					libraryCssFileList.Add(cssFile);
+				}
+				foreach(var cssFile in manifest.Module.Css) {
+					moduleCssFileList.Add(cssFile);
+				}
+				foreach(var jsFile in manifest.Libraries.Js) {
+					libraryJsFileList.Add(jsFile);
+				}
+				foreach(var jsFile in manifest.Module.Js) {
+					moduleJsFileList.Add(jsFile);
+				}
+				foreach(var appInject in manifest.Module.WVAppInjects) {
+					appDependencyInjections.Add(appInject);
+				}
+			}
+			#endregion
+
+
 			ViewBag.CacheBreaker = 20150920;
-			//pluginService.
 			ViewBag.Lang = Settings.Lang;
+			ViewBag.LibraryCssFileList = libraryCssFileList;
+			ViewBag.ModuleCssFileList = moduleCssFileList;
+			ViewBag.LibraryJsFileList = libraryJsFileList;
+			ViewBag.ModuleJsFileList = moduleJsFileList;
+			ViewBag.AppDependencyInjections = appDependencyInjections;
+
             return View();
         }
     }
