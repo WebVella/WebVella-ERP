279e35b9c63f4231f13c3fa772d0f447cae9630e
Boz Zashev <boz@webvella.com>
2016-05-17 17:53:15 +0300

    Implementing project configuration file (for the email settings)

--- WebVella.ERP.Project/EmailService.cs
+++ WebVella.ERP.Project/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,16 +15,21 @@ namespace WebVella.ERP.Projects
 
 		public EmailService()
 		{
+			var configurationBuilder = new ConfigurationBuilder()
+				.SetBasePath(PlatformServices.Default.Application.ApplicationBasePath)
+				.AddJsonFile("webvella-projects.config.json");
+			var configuration = configurationBuilder.Build();
+
 
 			_settings = new EmailSettings()
 			{
-			Enabled = true,
-			Server = "mail.gmail.com",
-			EnableSsl = false,
-			Port = 25,
-			UserName = "fake@gmail.com",
-			Password = "test",
-			DisplayName = "Peter Dorko"	
+			Enabled = Convert.ToBoolean(configuration["EmailSettings:enabled"]),
+			Server = configuration["EmailSettings:server"],
+			EnableSsl = Convert.ToBoolean(configuration["EmailSettings:enable_ssl"]),
+			Port = Convert.ToInt16(configuration["EmailSettings:port"]),
+			UserName = configuration["EmailSettings:username"],
+			Password = configuration["EmailSettings:password"],
+			DisplayName = configuration["EmailSettings:display_name"]	
 			};
 		}
 
@@ -35,29 +41,30 @@ namespace WebVella.ERP.Projects
 
 		public void SendEmail(string[] toEmailAddresses, string subject, string body)
 		{
-			MailMessage message = new MailMessage();
-			MailAddress sender = new MailAddress(_settings.UserName, _settings.DisplayName);
+			if(_settings.Enabled) {
+				MailMessage message = new MailMessage();
+				MailAddress sender = new MailAddress(_settings.UserName, _settings.DisplayName);
 
-			SmtpClient smtp = new SmtpClient()
-			{
-				Host = _settings.Server,
-				Port = _settings.Port,
-				EnableSsl = _settings.EnableSsl,
-				UseDefaultCredentials = false,
-				Credentials = new System.Net.NetworkCredential(_settings.UserName, _settings.Password),
-				DeliveryMethod = SmtpDeliveryMethod.Network
-			};
-			message.From = sender;
+				SmtpClient smtp = new SmtpClient()
+				{
+					Host = _settings.Server,
+					Port = _settings.Port,
+					EnableSsl = _settings.EnableSsl,
+					UseDefaultCredentials = false,
+					Credentials = new System.Net.NetworkCredential(_settings.UserName, _settings.Password),
+					DeliveryMethod = SmtpDeliveryMethod.Network
+				};
+				message.From = sender;
 
-			foreach (var strEmail in toEmailAddresses)
-				message.To.Add(new MailAddress(strEmail.Trim()));
+				foreach (var strEmail in toEmailAddresses)
+					message.To.Add(new MailAddress(strEmail.Trim()));
 
-			message.Subject = subject;
-			message.Body = body;
-			message.IsBodyHtml = true;
-			//smtp.Send(message);
-			smtp.SendMailAsync(message);
-
+				message.Subject = subject;
+				message.Body = body;
+				message.IsBodyHtml = true;
+				//smtp.Send(message);
+				smtp.SendMailAsync(message);
+			}
 		}
 
 	}
