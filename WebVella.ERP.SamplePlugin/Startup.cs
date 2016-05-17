2b2c71460181fd678673c454ef0f745ee15eb7af
Rumen Yankov <rumen.yankov@gmail.com>
2016-05-17 17:13:11 +0300

    add sample code how to read configuration in plugin startup

--- WebVella.ERP.SamplePlugin/Startup.cs
+++ WebVella.ERP.SamplePlugin/Startup.cs
@@ -1,14 +1,24 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.Diagnostics;
 using WebVella.ERP.Plugins;
 
 namespace WebVella.ERP.SamplePlugin
 {
 	[PluginStartup]
-    public class Startup
-    {
+	public class Startup
+	{
 		public void Start()
 		{
+			// Sample how to read configuration from erp config file
+			// you can use your own config 
+			var configurationBuilder = new ConfigurationBuilder()
+				.SetBasePath(PlatformServices.Default.Application.ApplicationBasePath)
+				.AddJsonFile("config.json");
+			var configuration = configurationBuilder.Build();
+
+
 			Debug.WriteLine("WebVella Sample Plugin start called");
 		}
-    }
+	}
 }
