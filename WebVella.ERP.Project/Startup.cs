f5109d5847578c8565079a4c8819b65432db5a44
Boz Zashev <boz@webvella.com>
2016-06-13 15:28:48 +0300

    Project changes

--- WebVella.ERP.Project/Startup.cs
+++ WebVella.ERP.Project/Startup.cs
@@ -167,6 +167,20 @@ namespace WebVella.ERP.Project
 							}
 						}
 
+						if (currentPluginSettings.Version < 20160613)
+						{
+							try
+							{
+								currentPluginSettings.Version = 20160613;
+								Patch160613(entMan, relMan, recMan, createSampleRecords);
+							}
+							catch (Exception ex)
+							{
+								var exception = ex;
+								throw ex;
+							}
+						}
+
 						#endregion
 
 						#region << 4. Save needed changes to the plugin setting data >>
--- WebVella.ERP.Web/wwwroot/plugins/webvella-core/providers/core.directive.js
+++ WebVella.ERP.Web/wwwroot/plugins/webvella-core/providers/core.directive.js
@@ -128,24 +128,24 @@
 //To use include in app "ngLoadScript"
 
 
-//(function (ng) {
-//    'use strict';
+(function (ng) {
+    'use strict';
 
-//    var app = ng.module('ngLoadScript', []);
+    var app = ng.module('ngLoadScript', []);
 
-//    app.directive('script', function() {
-//        return {
-//            restrict: 'E',
-//            scope: false,
-//            link: function(scope, elem, attr) {
-//                if (attr.type=='text/javascript-lazy') {
-//                    var code = elem.text();
-//                    var f = new Function(code);
-//                    f();
-//                }
-//            }
-//        };
-//    });
-//}(angular));
+    app.directive('script', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            link: function(scope, elem, attr) {
+                if (attr.type=='text/javascript-lazy') {
+                    var code = elem.text();
+                    var f = new Function(code);
+                    f();
+                }
+            }
+        };
+    });
+}(angular));
 
 //#endregion
