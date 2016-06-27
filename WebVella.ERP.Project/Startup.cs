131003e21351d2f62526b8aa021142ee45ae8908
Boz Zashev <boz@webvella.com>
2016-06-27 11:34:37 +0300

    development

--- WebVella.ERP.Project/Startup.cs
+++ WebVella.ERP.Project/Startup.cs
@@ -183,6 +183,20 @@ namespace WebVella.ERP.Project
 							}
 						}
 
+						if (currentPluginSettings.Version < 20160627)
+						{
+							try
+							{
+								currentPluginSettings.Version = 20160627;
+								Patch160627(entMan, relMan, recMan, createSampleRecords);
+							}
+							catch (Exception ex)
+							{
+								var exception = ex;
+								throw ex;
+							}
+						}
+
 						#endregion
 
 						#region << 4. Save needed changes to the plugin setting data >>
