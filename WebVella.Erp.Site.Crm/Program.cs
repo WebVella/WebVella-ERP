e85a9bd81f694160dd268efa15f56036815b23b2
Rumen Yankov <rumen.yankov@gmail.com>
2019-02-01 15:48:44 +0200

    adds mail plugin empty project and multiple websites with different configuration for plugin development

---
+++ WebVella.Erp.Site.Crm/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebVella.Erp.Site.Mail
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args) =>
+		   WebHost.CreateDefaultBuilder(args)
+			   .UseStartup<Startup>()
+			   .UseWebRoot(@"..\WebVella.Erp.Site\wwwroot")
+			   .Build();
+	}
+}
---
+++ WebVella.Erp.Site.Crm/Startup.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Compression;
+using WebVella.Erp.Plugins.Crm;
+using WebVella.Erp.Plugins.SDK;
+using WebVella.Erp.Web;
+using WebVella.Erp.Web.Middleware;
+
+namespace WebVella.Erp.Site
+{
+	public class Startup
+	{
+		public Startup()
+		{
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+			services.AddResponseCompression(options => { options.Providers.Add<GzipCompressionProvider>(); });
+			services.AddRouting(options => { options.LowercaseUrls = true; });
+
+			//CORS policy declaration
+			services.AddCors(options =>
+			{
+				options.AddPolicy("AllowNodeJsLocalhost",
+					builder => builder.WithOrigins("http://localhost:3000", "http://localhost").AllowAnyMethod().AllowCredentials());
+			});
+
+			services.AddDetectionCore().AddDevice();
+
+			services.AddMvc()
+
+				.AddRazorPagesOptions(options =>
+				{
+					options.Conventions.AuthorizeFolder("/");
+					options.Conventions.AllowAnonymousToPage("/login");
+				})
+				.AddJsonOptions(options =>
+				{
+					options.SerializerSettings.Converters.Add(new ErpDateTimeJsonConverter());
+				});
+
+			//adds global datetime converter for json.net
+			JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+			{
+				Converters = new List<JsonConverter> { new ErpDateTimeJsonConverter() }
+			};
+
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+					.AddCookie(options =>
+					{
+						options.Cookie.HttpOnly = true;
+						options.Cookie.Name = "erp_auth";
+						options.LoginPath = new PathString("/login");
+						options.LogoutPath = new PathString("/logout");
+						options.AccessDeniedPath = new PathString("/error?access_denied");
+						options.ReturnUrlParameter = "ret_url";
+					});
+
+			services.AddErp();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseRequestLocalization(new RequestLocalizationOptions
+			{
+				DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(CultureInfo.GetCultureInfo("en-US"))
+			});
+
+			app.UseAuthentication();
+
+			app
+			.UseErpPlugin<SdkPlugin>()
+			.UseErpPlugin<CrmPlugin>()
+			.UseErp()
+			.UseErpMiddleware();
+
+			//env.EnvironmentName = EnvironmentName.Production;
+			// Add the following to the request pipeline only in development environment.
+			if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				// Add Error handling middleware which catches all application specific errors and
+				// send the request to the following path or controller action.
+				app.UseErrorHandlingMiddleware();
+				app.UseExceptionHandler("/error");
+				app.UseStatusCodePagesWithReExecute("/error");
+			}
+
+			//Should be before Static files
+			app.UseResponseCompression();
+
+			app.UseCors("AllowNodeJsLocalhost"); //Enable CORS -> should be before static files to enable for it too
+
+			app.UseStaticFiles(new StaticFileOptions
+			{
+				OnPrepareResponse = ctx =>
+				{
+					const int durationInSeconds = 60 * 60 * 24 * 30; //30 days caching of these resources
+					ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+						"public,max-age=" + durationInSeconds;
+				}
+			});
+
+			app.UseMvc(routes => { routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}"); });
+		}
+	}
+}
+
