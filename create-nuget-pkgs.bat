01a79676733a5ce334172bd6fab43e7cc44fb3fc
Rumen Yankov <rumen.yankov@gmail.com>
2019-02-15 12:34:24 +0200

    adds nuspec file for Mail Plugin

--- create-nuget-pkgs.bat
+++ create-nuget-pkgs.bat
@@ -1,3 +1,4 @@
 nuget pack .\WebVella.Erp\WebVella.Erp.nuspec
 nuget pack .\WebVella.Erp.Web\WebVella.Erp.Web.nuspec
 nuget pack .\WebVella.Erp.Plugins.Sdk\WebVella.Erp.Plugins.Sdk.nuspec
+nuget pack .\WebVella.Erp.Plugins.Mail\WebVella.Erp.Plugins.Mail.nuspec
