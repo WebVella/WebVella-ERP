d9832fc1bada1c059ef80e1c2ca51a8d07870e59
Rumen Yankov <rumen.yankov@gmail.com>
2019-02-25 16:57:48 +0200

    tmp impl

---
+++ WebVella.Erp/Recurrence/RecurrenceRepeatMonthType.cs
@@ -0,0 +1,12 @@
+﻿using WebVella.Erp.Api.Models;
+
+namespace WebVella.Erp.Recurrence
+{
+	public enum RecurrenceRepeatMonthType
+	{
+		[SelectOption(Label = "by day")]
+		ByDate = 0,
+		[SelectOption(Label = "by week day")]
+		ByWeekDay = 1
+	}
+}
