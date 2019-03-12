e3d2761362454c88c1e5299f59ab8025abc269d5
Rumen Yankov <rumen.yankov@gmail.com>
2019-03-12 08:44:10 +0200

    fixes page form data conversion to entity record

--- WebVella.Erp.Web/Services/PageService.cs
+++ WebVella.Erp.Web/Services/PageService.cs
@@ -1401,12 +1401,12 @@ namespace WebVella.Erp.Web.Services
                 {
                     string[] fieldSect = key.Split(".");
                     if (fieldSect.Length < 2)
-                        throw new Exception($"'{key}' is not valid relational field.");
+						continue; //ignore this field
 
-                    var relationName = fieldSect[0].Substring(1);
+					var relationName = fieldSect[0].Substring(1);
                     var relation = entityRelations.SingleOrDefault(x => x.Name.ToLowerInvariant() == relationName.ToLowerInvariant());
-                    if (relation == null)
-                        throw new Exception($"'{key}' relation is not found.");
+					if (relation == null)
+						continue; //ignore this field
 
                     Entity relatedEntity = null;
                     if (relation.OriginEntityId == entity.Id)
@@ -1416,9 +1416,9 @@ namespace WebVella.Erp.Web.Services
 
                     Field field = relatedEntity.Fields.SingleOrDefault(x => x.Name == fieldSect[1]);
                     if (field == null)
-                        throw new Exception($" Field '{fieldSect[1]}' not found in entity '{relatedEntity.Name}'.");
+						continue; //ignore this field
 
-                    fieldTypeDictionary[key] = FieldType.RelationField;
+					fieldTypeDictionary[key] = FieldType.RelationField;
                     relationsFieldTypeDictionary[key] = field.GetFieldType();
                 }
 
