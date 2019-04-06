2aca6eb17dfcf449a50246c3e6955c67c047b6ad
Boz Zashev <boz@webvella.com>
2019-04-06 09:50:16 +0300

    Fixing fixed page header and general layout changers

--- WebVella.Erp.Web/TagHelpers/WvPageHeader/script.js
+++ WebVella.Erp.Web/TagHelpers/WvPageHeader/script.js
@@ -2,14 +2,37 @@
 
 function WebVellaErpWebComponentsPcPageHeader_Init(elementId) {
     var elementHtmlId = '#wv-' + elementId;
+    var fakeElementId = "wv-fake-" + elementId;
     var elementPosition = $(elementHtmlId).offset();
+    var elementHeight = $(elementHtmlId).outerHeight();
 
-    $("#body-inner-wrapper-2").scroll(function () {
-        if ($("#body-inner-wrapper-2").scrollTop() > elementPosition.top) {
-            $(elementHtmlId).addClass('fixed');
+    //$("#body-inner-wrapper-2").scroll(function () {
+    //    if ($("#body-inner-wrapper-2").scrollTop() > elementPosition.top) {
+    //        //Append fake element to preserve height
+    //        if (!$(elementHtmlId).hasClass("fixed")) {
+    //            $("<div/>").css("height", elementHeight).attr("id", fakeElementId).insertAfter(elementHtmlId);
+    //            $(elementHtmlId).addClass('fixed');
+    //        }
+    //    } else {
+    //        if ($(elementHtmlId).hasClass("fixed")) {
+    //            $("#" + fakeElementId).remove();
+    //            $(elementHtmlId).removeClass('fixed');
+    //        }
+    //    }
+    //});    	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > elementPosition.top) {
+            //Append fake element to preserve height
+            if (!$(elementHtmlId).hasClass("fixed")) {
+                $("<div/>").css("height", elementHeight).attr("id", fakeElementId).insertAfter(elementHtmlId);
+                $(elementHtmlId).addClass('fixed');
+            }
         } else {
-            $(elementHtmlId).removeClass('fixed');
+            if ($(elementHtmlId).hasClass("fixed")) {
+                $("#" + fakeElementId).remove();
+                $(elementHtmlId).removeClass('fixed');
+            }
         }
-    });    	
+    });  
 }
 
