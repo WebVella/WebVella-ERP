ec0f3c2413330be237505dacd5e37c8f7a322e99
Boz Zashev <boz@webvella.com>
2019-12-02 12:09:47 +0200

    WvJsonRaw implementation

---
+++ WebVella.Erp.Web/Utils/HtmlHelperExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace System.Web.Mvc
+{
+	public static class WvTaghelperExtension
+	{
+		public static IHtmlContent WvJsonRaw(this IHtmlHelper helper, string input)
+		{
+
+			return new HtmlString(helper.Raw(input).ToString().Replace("</script>", "</s\\cript>"));
+		}
+	}
+}
