c911062c77d23c7d33740a293505ff40aa5fb111
Rumen Yankov <rumen.yankov@gmail.com>
2020-08-19 10:48:51 +0300

    fix for blazor authentication

--- WebVella.Erp.Web/Middleware/SecuritityCircuitHandler.cs
+++ WebVella.Erp.Web/Middleware/SecuritityCircuitHandler.cs
@@ -1,36 +1,47 @@
-﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.Circuits;
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using WebVella.Erp.Api.Models;
 using WebVella.Erp.Database;
+using WebVella.Erp.Web.Services;
 
 namespace WebVella.Erp.Web.Middleware
 {
-    public class SecuritityCircuitHandler : CircuitHandler
-    {
-        private Dictionary<Circuit, Tuple<IDisposable, IDisposable>> contexts = new Dictionary<Circuit, Tuple<IDisposable, IDisposable>>();
+	public class SecuritityCircuitHandler : CircuitHandler
+	{
+		private AuthenticationStateProvider authStateProvider = null;
 
-        public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
-        {
-            IDisposable dbCtx = DbContext.CreateContext(ErpSettings.ConnectionString);
-            IDisposable secCtx = WebVella.Erp.Api.SecurityContext.OpenSystemScope();
-            contexts.Add(circuit, new Tuple<IDisposable, IDisposable>(dbCtx, secCtx));
-            return Task.CompletedTask;
-        }
+		private Dictionary<Circuit, Tuple<IDisposable, IDisposable>> contexts = new Dictionary<Circuit, Tuple<IDisposable, IDisposable>>();
 
-        public override Task OnConnectionDownAsync(Circuit circuit, CancellationToken cancellationToken)
-        {
-            if (contexts.ContainsKey(circuit))
-            {
-                Tuple<IDisposable, IDisposable> tuple = contexts[circuit];
-                tuple.Item1.Dispose();
-                tuple.Item2.Dispose();
-                contexts.Remove(circuit);
-            }
-            return Task.CompletedTask;
-        }
+		public SecuritityCircuitHandler(AuthenticationStateProvider authStateProvider)
+		{
+			this.authStateProvider = authStateProvider;
+		}
 
-        public int ConnectedCircuits => contexts.Count;
-    }
+		public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
+		{
+			IDisposable dbCtx = DbContext.CreateContext(ErpSettings.ConnectionString);
+			ErpUser user = AuthService.GetUser(authStateProvider.GetAuthenticationStateAsync().Result.User);
+			IDisposable secCtx = user != null ? WebVella.Erp.Api.SecurityContext.OpenScope(user) : WebVella.Erp.Api.SecurityContext.OpenSystemScope();
+			contexts.Add(circuit, new Tuple<IDisposable, IDisposable>(dbCtx, secCtx));
+			return Task.CompletedTask;
+		}
+
+		public override Task OnConnectionDownAsync(Circuit circuit, CancellationToken cancellationToken)
+		{
+			if (contexts.ContainsKey(circuit))
+			{
+				Tuple<IDisposable, IDisposable> tuple = contexts[circuit];
+				tuple.Item1.Dispose();
+				tuple.Item2.Dispose();
+				contexts.Remove(circuit);
+			}
+			return Task.CompletedTask;
+		}
+
+		public int ConnectedCircuits => contexts.Count;
+	}
 }
