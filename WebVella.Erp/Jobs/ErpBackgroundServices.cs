e2856f8cbdffe2e5df291b6b9f6cf8292701440a
Rumen Yankov <rumen.yankov@gmail.com>
2020-08-19 15:31:40 +0300

    fixes problems with background services order of start using WebHost or Generic Host

--- WebVella.Erp/Jobs/ErpBackgroundServices.cs
+++ WebVella.Erp/Jobs/ErpBackgroundServices.cs
@@ -4,31 +4,37 @@ using System.Threading.Tasks;
 
 namespace WebVella.Erp.Jobs
 {
-    public class ErpJobScheduleService : BackgroundService
-    {
-        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            ScheduleManager.Current.ProcessSchedulesAsync(stoppingToken);
-            await Task.FromResult(0);
-        }
+	public class ErpJobScheduleService : BackgroundService
+	{
+		protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (ScheduleManager.Current == null)
+				await Task.Delay(1000);
 
-        public override async Task StopAsync(CancellationToken stoppingToken)
-        {
-            await Task.FromResult(0);
-        }
-    }
+			ScheduleManager.Current.ProcessSchedulesAsync(stoppingToken);
+			await Task.FromResult(0);
+		}
 
-    public class ErpJobProcessService : BackgroundService
-    {
-        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            JobManager.Current.ProcessJobsAsync(stoppingToken);
-            await Task.FromResult(0);
-        }
+		public override async Task StopAsync(CancellationToken stoppingToken)
+		{
+			await Task.FromResult(0);
+		}
+	}
 
-        public override async Task StopAsync(CancellationToken stoppingToken)
-        {
-            await Task.FromResult(0);
-        }
-    }
+	public class ErpJobProcessService : BackgroundService
+	{
+		protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (JobManager.Current == null)
+				await Task.Delay(1000);
+
+			JobManager.Current.ProcessJobsAsync(stoppingToken);
+			await Task.FromResult(0);
+		}
+
+		public override async Task StopAsync(CancellationToken stoppingToken)
+		{
+			await Task.FromResult(0);
+		}
+	}
 }
